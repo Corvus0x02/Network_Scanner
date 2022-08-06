6aff984390425deb93c0a81a52f3777dfd447426
Corvus0x02 <Corvus0x02@protonmail.com>
2022-08-06 14:20:57 -0400

    08062022

--- network_scanner.py
+++ network_scanner.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 import scapy.all as scapy
+
 def scan(ip):
-    scapy.arping(ip)
-scan("IP")
+    #Create ARP packet
+    arp_request = scapy.ARP(pdst=ip)
+    #Create an Ethernet object
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    #Combine the broadcast and ARP request packets
+    arp_request_broadcast = broadcast/arp_request
+    #Send packets with a custom Ether part. Returns two lists answered and unanswered
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+    #print(answered_list.summary())
+    for element in answered_list:
+        print(element)
+
+scan("IP/Subnet")
